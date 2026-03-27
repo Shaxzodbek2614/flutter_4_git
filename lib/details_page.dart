bf20668f6f7920b5a75b337ca37e0b6e1c9c025a
Shaxzodbek2614 <shaxzodbek1703@gmail.com>
2026-03-27 20:04:53 +0500

    appBarni qo'shdim

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -5,6 +5,8 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("salom"),),
+    );
   }
 }
