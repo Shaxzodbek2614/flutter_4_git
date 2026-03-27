65eb08a8fbd7e908e40ec8eb6d988dcc772de14f
Shaxzodbek2614 <shaxzodbek1703@gmail.com>
2026-03-27 18:45:06 +0500

    salom

--- lib/third.dart
+++ lib/third.dart
@@ -7,6 +7,7 @@ class Third extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("salom"),),
+      backgroundColor: Colors.red,
     );
   }
 }
